dc2fc305d7f64217948e4ff6b7612a91e0f617df
Anghelo1010 <anghelo.pecho@ucb.edu.bo>
2026-03-22 20:29:05 -0400

    cambios
    agrego de librerias

--- service-auth/users/views.py
+++ service-auth/users/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import User, Role, Permission
+from django.core.cache import cache
 from .serializers import (
     UserSerializer,
     UserCreateSerializer,
