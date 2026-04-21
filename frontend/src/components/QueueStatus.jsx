2058c4f606f5e3be3124106c5d7089c5267567d6
AlexQuinoMiranda <alexquinom24@gmail.com>
2026-04-20 23:53:11 -0400

    agregar placeholders de posicion y tiempo

---
+++ frontend/src/components/QueueStatus.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function QueueStatus() {
+  return (
+    <div>
+      <h2>Fila virtual</h2>
+      <p>Cargando información de la cola...</p>
+      <p>Posición en cola: --</p>
+      <p>Tiempo estimado: --</p>
+    </div>
+  );
+}
