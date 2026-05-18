3740191ced05240bcc49701e9b90e5f54276dea8
Anghelo1010 <anghelo.pecho@ucb.edu.bo>
2026-05-18 11:43:57 -0400

    se trabajo en la sub tarea. Backend: Endpoint PUT /users/{id}/notification-preferences para que el usuario configure qué categorías le generan notificaciones [2h]
    se trabajo en la sub tarea. Backend: Endpoint PUT /users/{id}/notification-preferences para que el usuario configure qué categorías le generan notificaciones [2h]

--- service-events/events/apps.py
+++ service-events/events/apps.py
@@ -4,8 +4,5 @@ from django.apps import AppConfig
 class EventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
-    verbose_name = 'Events Management'
-
     def ready(self):
-        """Registra los signals al arrancar la app (TIC-373)."""
-        import events.signals  # noqa: F401
+        import events.signals
